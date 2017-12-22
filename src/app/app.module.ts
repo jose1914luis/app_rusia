75b94bf00cc9107e322693ea821c36427c960e12
jose luis <jose1914luis@gmail.com>
2017-12-22 13:17:30 -0500

    my notes2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,12 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { ListPage } from '../pages/list/list';
+import { CrearCuentaPage } from '../pages/crear-cuenta/crear-cuenta';
+
+import { Network } from '@ionic-native/network';
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,11 +22,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    CrearCuentaPage,
+    ListPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot() 
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,10 +37,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    CrearCuentaPage,
+    ListPage
   ],
   providers: [
     StatusBar,
+    Network,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
