67c879b73ca8d4ea8ff1a57b87e56b02b5b9dbbc
jose luis <jose1914luis@gmail.com>
2017-12-22 13:08:19 -0500

    my notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { ListPage } from '../pages/list/list';
+import { CrearCuentaPage } from '../pages/crear-cuenta/crear-cuenta';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = ListPage;
+  pages: Array<{title: string, component: any}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
+    // used for an example of ngFor and navigation
+    /*this.pages = [
+      { title: 'Mis eventos', component: HomePage },
+      { title: 'Proximos Tours', component: ProximoPage },
+      { title: 'Punto de Encuentro', component: PuntoPage },
+      { title: 'Mapas', component: MapaPage },
+      { title: 'Promociones', component: PromocionPage },
+      { title: 'Preguntas F.A.Q.', component: FaqPage },
+      { title: 'Contacto', component: ContactoPage },
+      { title: 'Mi perfil', component: PerfilPage }
+    ];*/
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
